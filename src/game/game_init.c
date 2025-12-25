a889f25a60bd5d85d196813d599f00b4a858f957
SethLJ <spidersoftpretzel@gmail.com>
2025-12-24 20:10:04 -0500

    Ensure file selections register without relying on animation finish

--- src/game/game_init.c
+++ src/game/game_init.c
@@ -646,8 +646,6 @@ void setup_game_memory(void) {
  */
 void thread5_game_loop(UNUSED void *arg) {
     struct LevelCommand *addr;
-    u8 interpolationToggle = FALSE;
-
     CN_DEBUG_PRINTF(("start gfx thread\n"));
 
     setup_game_memory();
@@ -675,10 +673,9 @@ void thread5_game_loop(UNUSED void *arg) {
     render_init();
 
     while (TRUE) {
-        gIsFrameInterpolated = interpolationToggle;
-        gFrameInterpolation = gIsFrameInterpolated ? 0.5f : 0.0f;
-        gInterpolatingSurfaces = gIsFrameInterpolated;
-        interpolationToggle ^= 1;
+        gIsFrameInterpolated = FALSE;
+        gFrameInterpolation = 1.0f;
+        gInterpolatingSurfaces = FALSE;
 
         // If the reset timer is active, run the process to reset the game.
         if (gResetTimer != 0) {
